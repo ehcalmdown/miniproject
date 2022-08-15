46d6ddd7623935a16b03b487144c6388c5945c5a
JJ How <holyyamateh@hotmail.sg>
2022-08-15 15:40:04 +0800

    initial

---
+++ src/main/java/sg/nus/iss/miniproject69/Miniproject69Application.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.miniproject69;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniproject69Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniproject69Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/iss/miniproject69/Miniproject69ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.miniproject69;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniproject69ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
